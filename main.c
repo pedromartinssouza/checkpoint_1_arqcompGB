7827a091c4cee56d445043675c5489b54c7d2e3d
souza <pedromartins.souza1999@gmail.com>
2021-11-03 22:14:25 -0300

    Fix calc_adjustment and rename to get_adjustment

--- main.c
+++ main.c
@@ -23,71 +23,31 @@ typedef struct input {
 } input_t;
 
 void print_binary(int bin_number, int size);
-int calc_adjustment(int bin_number1, int bin_number2);
+int get_adjustment(int bin_number1, int bin_number2);
 float reconstructor(union_float x);
-void ieee754_printer(union_float x);
+void ieee754_print(union_float x);
+
+union_float sum(input_t var1, input_t var2);
+
+
 
 int main()
 {
     input_t var1;
     input_t var2;
 
-    int shift_value;
-    int sum_mantissa;//teste
-    int adjustment;
-    union_float sum_result;
+    union_float result;
 
     printf("Enter first number: ");
     scanf("%f", &var1.number_input.f);
     printf("Enter second number: ");
     scanf("%f", &var2.number_input.f);
 
-    ieee754_printer(var1.number_input);
-    ieee754_printer(var2.number_input);
+    ieee754_print(var1.number_input);
+    ieee754_print(var2.number_input);
 
-    var1.exp = var1.number_input.field.power - 127;
-    var2.exp = var2.number_input.field.power - 127;
-
-    var1.mantissa_aux = var1.number_input.field.mantissa + 0x800000;
-    var2.mantissa_aux = var2.number_input.field.mantissa + 0x800000;
-
-    if (var1.exp != var2.exp)
-    {
-        if(var1.exp>var2.exp)
-        {
-            shift_value = var1.exp - var2.exp;
-            var2.mantissa_aux = var2.mantissa_aux >> shift_value;
-            adjustment = calc_adjustment(var1.mantissa_aux, var2.mantissa_aux);
-
-            sum_mantissa = (var2.mantissa_aux >> adjustment) + (var1.mantissa_aux >> adjustment);
-
-            sum_result.field.power = var1.number_input.field.power + adjustment;
-            sum_result.field.signal = var1.number_input.field.signal;
-        } else
-        {
-            shift_value = var2.exp - var1.exp;
-            var1.mantissa_aux = var1.mantissa_aux >> shift_value;
-            adjustment = calc_adjustment(var1.mantissa_aux, var2.mantissa_aux);
-
-            sum_mantissa = (var1.mantissa_aux >> adjustment) + (var2.mantissa_aux >> adjustment);
-
-            sum_result.field.power = var2.number_input.field.power + adjustment;
-            sum_result.field.signal = var2.number_input.field.signal;
-        }
-
-        sum_result.field.mantissa = sum_mantissa;
-        ieee754_printer(sum_result);
-    } else
-    {
-        adjustment = calc_adjustment(var1.mantissa_aux, var2.mantissa_aux);
-        sum_mantissa = (var1.mantissa_aux >> adjustment) + (var2.mantissa_aux >> adjustment);
-        sum_result.field.power = var1.number_input.field.power + adjustment;
-        sum_result.field.signal = var1.number_input.field.signal;
-        sum_result.field.mantissa = sum_mantissa;
-
-        ieee754_printer(sum_result);
-    }
-
+    result = sum(var1, var2);
+    ieee754_print(result);
     return 0;
 }
 
@@ -105,18 +65,12 @@ void print_binary(int bin_number, int size)
     }
 }
 
-// calc_adjustment finds out what is the necessary shift value to normalize a number
-// the logic consists of examining how many 1's there are after running an AND operation against the two numbers
-// to be added
-int calc_adjustment(int bin_number1, int bin_number2)
+// get_adjustment returns the amount of necessary shifts to normalize the sum of two binary numbers
+int get_adjustment(int bin_number1, int bin_number2)
 {
-    int bin_number = bin_number1 & bin_number2;
-    int aux = 0;
-    while ((bin_number << aux) & 0x800000) // while first number of shifted bin_number is 1, stay in loop (bin 1 followed by 23 zeros)
-    {
-        aux++;
-    }
-    return aux;
+    if (bin_number1 & bin_number2 & 0x800000)
+        return 1;
+    return 0;
 }
 
 // reconstructor manually forms the float version of a IEE754 binary
@@ -125,8 +79,8 @@ float reconstructor(union_float x)
     return pow(-1,(x.field.signal)) * ( 1.0 + x.field.mantissa / pow(2,23)) * pow(2,(x.field.power - 127));
 }
 
-// ieee754_printer prints relevant data about a IEE754 binary
-void ieee754_printer(union_float x)
+// ieee754_print prints relevant data about a IEE754 binary
+void ieee754_print(union_float x)
 {
     printf("\n\n");
     printf("%d ", x.field.signal);
@@ -135,4 +89,54 @@ void ieee754_printer(union_float x)
     print_binary(x.field.mantissa, 23);
     printf("\n");
     printf("Number reconstructed: %f \n", reconstructor(x));
+}
+
+union_float sum(input_t var1, input_t var2)
+{
+    int shift_value;
+    int sum_mantissa;//teste
+    int adjustment = 0;
+    union_float sum_result;
+
+    var1.exp = var1.number_input.field.power - 127;
+    var2.exp = var2.number_input.field.power - 127;
+
+    var1.mantissa_aux = var1.number_input.field.mantissa + 0x800000;
+    var2.mantissa_aux = var2.number_input.field.mantissa + 0x800000;
+
+    if (var1.exp != var2.exp)
+    {
+        if(var1.exp>var2.exp)
+        {
+            shift_value = var1.exp - var2.exp;
+            var2.mantissa_aux = var2.mantissa_aux >> shift_value;
+            adjustment = get_adjustment(var1.mantissa_aux, var2.mantissa_aux);
+
+            sum_mantissa = (var2.mantissa_aux >> adjustment) + (var1.mantissa_aux >> adjustment);
+
+            sum_result.field.power = var1.number_input.field.power + adjustment;
+            sum_result.field.signal = var1.number_input.field.signal;
+        } else
+        {
+            shift_value = var2.exp - var1.exp;
+            var1.mantissa_aux = var1.mantissa_aux >> shift_value;
+            adjustment = get_adjustment(var1.mantissa_aux, var2.mantissa_aux);
+
+            sum_mantissa = (var1.mantissa_aux >> adjustment) + (var2.mantissa_aux >> adjustment);
+
+            sum_result.field.power = var2.number_input.field.power + adjustment;
+            sum_result.field.signal = var2.number_input.field.signal;
+        }
+
+        sum_result.field.mantissa = sum_mantissa;
+    } else
+    {
+        adjustment = get_adjustment(var1.mantissa_aux, var2.mantissa_aux);
+        sum_mantissa = (var1.mantissa_aux >> adjustment) + (var2.mantissa_aux >> adjustment);
+        sum_result.field.power = var1.number_input.field.power + adjustment;
+        sum_result.field.signal = var1.number_input.field.signal;
+        sum_result.field.mantissa = sum_mantissa;
+    }
+
+    return sum_result;
 }
